f8e600eed5adfee0c169eee0e56a89edd63e2f11
alibaba krol kebaba <kamilziolkowski231@gmail.com>
2025-10-20 00:09:56 +0200

    feat: name for unit shouldnt be null

--- backend/Receipts/Model/Unit.cs
+++ backend/Receipts/Model/Unit.cs
@@ -1,7 +1,12 @@
-﻿namespace inzynierka.Receipts.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace inzynierka.Receipts.Model;
 
 public class Unit
 {
+    [Key]
     public int UnitId { get; set; }
+    [Required]
+    [MinLength(1)]
     public string Name { get; set; }
 }
